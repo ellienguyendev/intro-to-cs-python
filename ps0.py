9966ff115569e9aba80f56e38c0aa44994384e24
Ellie Nguyen <ellienguyendev@gmail.com>
2020-08-24 21:26:32 -0700

    changed comments

--- ps0.py
+++ ps0.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 21 15:33:17 2020
 
 @author: Ellie
 """
 
 # Write a program that does the following in order:
-# 1. Asks the user to enter a number “x” 
+# 1. Asks the user to enter a number “x”
 # 2. Asks the user to enter a number “y”
-# 3. Prints out number “x”, raised to the power “y”. 
+# 3. Prints out number “x”, raised to the power “y”.
 # 4. Prints out the log (base 2) of “x”.
 
-# Users should see: 
-# Enter number x: 2 
-# Enter number y: 3 
+# Users should see:
+# Enter number x: 2
+# Enter number y: 3
 # x**y = 8
 # log(x) = 1
 
