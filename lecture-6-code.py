7c764423927376fbd6ca7e052c5e00c5f41ed29e
Ellie Nguyen <ellienguyendev@gmail.com>
2020-09-15 23:22:24 -0700

    lecture 6 code practice

--- lecture-6-code.py
+++ lecture-6-code.py
@@ -5,6 +5,32 @@
 Lecture 6 Code and Practice Exercises
 """
 
+# Factorials
+
+# Iterative Factorial
+
+def factI(n):
+    """Assumes n an int > 0 
+       Returns n!"""
+       
+    result = 1
+    while n > 1:
+        result *= n 
+        n -= 1
+    return result
+        
+# Recursive Factorial
+def factR(n):
+    """Assume n an int > 0 
+       Returns n!"""
+       
+    if n == 1:
+        return n
+    else:
+        return n*factR(n-1)
+
+# Fibonacci Sequence
+
 def fib(n):
     """Assume n int >= 0
         Return Fibonacci of n"""
@@ -36,6 +62,25 @@ def fib(n):
 
 fib(5)
 
+# Fibonacci using dictonaries 
+
+def fib_efficient(n, d):
+    """Assume n int >= 0
+       Assume d dict n: fibonacci of n as key: value pairs
+        Return Fibonacci of n"""
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d)+fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
+        
+d = {1:1, 2:2}
+
+argToUse = 34
+print(fib_efficient(argToUse, d))
+
+
 # Check if a string is a palindrome
 
 def isPalindrom(s):
@@ -59,3 +104,102 @@ def isPalindrom(s):
             return s[0] == s[1] and isPal(s[1:-1])
 
     return isPal(toChar(s))
+
+# Evaluate the lyrics of a song and create a dictionary with how many times each word appears in the song. Then create another function that outputs the most frequent words. Then create a function that outputs how often a word appears a minimum number of times.
+
+lyric_string = "were just ordinary people we dont know which way to go cause were ordinary people maybe we should take it slow take it slow ohh this time well take it slow take it slow, ohh this time well take it slow"
+
+lyrics_list = lyric_string.split()
+
+def lyrics_to_dict(lyrics):
+    """"Assume lyrics a list with strings with no special        characters
+        Return dictionary with every unique word: how many times it appears in the lyrics"""
+    
+    myDict = {}
+    
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
+
+lyrics_to_dict(lyrics_list)
+
+
+def most_common_words(freqs):
+    """"Assume freqs a dictionary with words: frequency appeared in a song as the key: value pairs
+        Return most common words"""
+    
+    values = freqs.values()
+    highest = max(values)
+    words = []
+    
+    for k in freqs:
+        if freqs[k] == highest:
+            words.append(k)
+    return (words, highest)
+
+most_common_words(lyrics_to_dict(lyrics_list))
+
+
+def words_often(freqs, minTimes):
+    """"Assume freqs a dictionary with words: frequency appeared in a song as the key: value pairs
+        Assume int > 0
+        Return words that appear >= minTimes"""
+        
+    result = []
+    done = False
+    
+    while not done:
+        temp = most_common_words(freqs)
+        print(temp)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+    return result
+    
+words_often(lyrics_to_dict(lyrics_list), 4)
+
+
+# Towers of Hanoi
+
+def printMove(fr,to):
+    print('move from ' + str(fr) + ' to ' + str(to))
+    
+def Towers(n,fr,to,spare):
+    if n == 1:
+        printMove(fr,to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+        
+Towers(4, 'original', 'destination', 'spare')
+
+
+# Iterative solution for multiplication
+
+def multI(a,b):
+    result = 0 
+    
+    while b > 0:
+        result +=a
+        b -= 1
+    return result
+
+multI(4,6)
+
+# Recursive solution for multiplication
+
+def multR(a,b):
+    if b == 1:
+        return a
+    else:
+        return a + multR(a, b-1)
+
+multR(4,6)
+
